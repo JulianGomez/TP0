bf6220422dbdc0e5aaa5b76e80862f931bc3d08f
JulianGomez <juulian_gomez@yahoo.com.ar>
2016-02-24 16:36:22 -0300

    Commit 24/02/2016 - agregado de retorno de la función
    Agregado al tp0  "hello, word" retorno de la función

--- Jgomez TP0 CodigoFuente.c
+++ Jgomez TP0 CodigoFuente.c
@@ -1,11 +1,18 @@
 /* Hola Mundo!
 * Julian Gomez 153.605-9
-* 20150405
+* 20160224
 */
 
 #include <stdio.h>
 
-int main(void){
- printf("Hola Mundo! \n");
+
+
+int main(void)
+{
+   
+printf("Hello, World!\n");
+    
+return 0;
+
 }
 
